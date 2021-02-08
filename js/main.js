646dcf106329d3cccc6a9b8886d00d284569f71a
henrydinkel-s <69637885+henrydinkel-s@users.noreply.github.com>
2021-02-08 17:29:04 +0000

    Uploaded files

---
+++ js/main.js
@@ -0,0 +1,4 @@
+//Scroll position
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  }, false);
